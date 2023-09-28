90b4f15a752145a5551345914772cb2d5bad28aa
mikaelleRubia <mikaelle.rubia@outlook.com>
2023-09-28 11:32:44 -0300

    pontos e poligono

---
+++ aulaRevisao/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+class Poligono{
+    private:
+
+};
+
+class Ponto{
+    private:
+        float x, y;
+
+    public:
+        Ponto(float y, float x);
+
+        float getX();
+        void setX(float _x);
+
+        float getY();
+        void setY(float _y);      
+};
+
+
+int main(){
+    
+
+
+
+    
+    return 0;
+}
