99393fe16afe8e58c3d616f85ddbb88b017cc002
mikaelleRubia <mikaelle.rubia@outlook.com>
2023-10-05 19:02:30 -0300

    feature: função remover_aluno, add_aluno

--- aula22/nota_aluno.cpp
+++ aula22/nota_aluno.cpp
@@ -18,7 +18,7 @@ class Aluno{
 
         }
 
-        Aluno(string _nome, string _email, float _nota1, float _nota2){
+        Aluno(string _nome, string _email, double _nota1, double _nota2){
             nome = _nome;
             email = _email;
             nota1 = _nota1;
@@ -42,30 +42,32 @@ class Aluno{
             email = _email;
         }
 
-        float getNota1(){
+        double getNota1(){
             return nota1;
         }
 
-        void setNota1(float _nota1){
+        void setNota1(double _nota1){
             nota1 = _nota1;
         }
 
 
-        float getNota2(){
+        double getNota2(){
             return nota2;
         }
 
-        void setNota2(float _nota2){
+        void setNota2(double _nota2){
             nota2 = _nota2;
         }
 
         static void add_alunos(vector<Aluno> &alunos);
+        static void altera_aluno(vector<Aluno> &alunos);
+        static bool remover_aluno(vector<Aluno> &aluno);
 };
 
 void Aluno::add_alunos(vector<Aluno> &alunos){
     Aluno novo_aluno;
     string add_nome, add_email;
-    float add_nota1, add_nota2;
+    double add_nota1, add_nota2;
 
     cout << "digite o nome" << endl;
     cin >> add_nome;
@@ -85,11 +87,57 @@ void Aluno::add_alunos(vector<Aluno> &alunos){
     novo_aluno.setNota2(add_nota2);
 
     alunos.push_back(novo_aluno);
-   
-    
-
 };
 
+void Aluno::altera_aluno(vector<Aluno> &aluno){
+    string nome;
+
+    cout << "Digite o nome do aluno que deseja atualizar os dados" << endl;
+    cin >> nome;
+
+    for(int i = 0; i < aluno.size(); i++){
+        if(aluno[i].nome == nome){
+        cout << "digite o nome atualizado " << endl;
+        cin >> aluno[i].nome;
+
+        cout << "digite o email atualizado" << endl;
+        cin >> aluno[i].email;
+
+        cout << "digite o nota1" << endl;
+        cin >> aluno[i].nota1;
+
+        cout << "digite o nota2" << endl;
+        cin >> aluno[i].nota2;
+        }
+    }
+};
+
+bool Aluno::remover_aluno(vector<Aluno> &aluno)
+{
+    string nome_;
+    int opcao;
+
+    cout << "Digite o nome do aluno que deseja remover" << endl;
+    cin >> nome_;
+
+    for (int i = 0; i < aluno.size(); i++){
+        if (aluno[i].nome == nome_){
+            cout << "======================================\n\n";
+            cout << "Deseja realmente remover o roteiro?\n";
+            cout << "1 - Sim\n";
+            cout << "2 - Nao\n";
+            cout << "OBS: Digite 1 ou 2, caso contrario sera considerado como Nao!\n";
+            cin >> opcao;
+
+            if (opcao == 1)
+            {
+                aluno.erase(aluno.begin() + i);
+                return true;
+            }
+        }
+    }
+    return false;
+}
 class BancoDeDados{
     public:
         static bool saveDados(vector <Aluno> alunos);
@@ -120,9 +168,11 @@ vector<Aluno> BancoDeDados::recuperarDados(){
             double nota1, nota2;
             getline(ss, nome, ',');
             getline(ss, email, ',');
-            ss >> nota1 >> nota2;
+            ss >> nota1;
+            ss.ignore();   // Ignora a vírgula
+            ss >> nota2;
 
-            alunos.push_back(Aluno(nome, email,nota1,nota2));
+            alunos.push_back(Aluno(nome, email, nota1, nota2));
         }
      arquivo.close();
     }
@@ -134,6 +184,7 @@ vector<Aluno> BancoDeDados::recuperarDados(){
 
 int main() {
     vector <Aluno> alunos;
+
     char resposta;
 
     // Aluno novo_aluno("Joao", "joao@email", 10, 10);
@@ -145,6 +196,7 @@ int main() {
     // alunos.push_back(novo_aluno1);
     // alunos.push_back(novo_aluno2);
     // alunos.push_back(novo_aluno3);
+
 
     do{
         Aluno::add_alunos(alunos);
@@ -159,9 +211,54 @@ int main() {
 
     alunos = BancoDeDados::recuperarDados();
     cout << "Alunos: " << endl;
-    for(Aluno alunos : alunos){
-        cout << alunos.getNome() << endl;
+    for(Aluno aluno : alunos){
+        cout << aluno.getNome() << ", " << aluno.getEmail() << ", " 
+            << aluno.getNota1() << ", " << aluno.getNota2() << endl;
     }
+
+
+    do{
+        cout << "------------------------------------" << endl;
+
+        Aluno::altera_aluno(alunos);
+
+        BancoDeDados::saveDados(alunos);
+        cout << "Dados atualizados" << endl;
+        cout << "------------------------------------" << endl;
+
+        cout << "Alunos atualizado " << endl;
+        for(Aluno aluno : alunos){
+            cout << aluno.getNome() << ", " << aluno.getEmail() << ", " 
+                << aluno.getNota1() << ", " << aluno.getNota2() << endl;
+        }
+
+        cout << "deseja alterar os dados de outro aluno?" << endl;
+        cin >> resposta;
+
+    }while(resposta == 's');
+
+
+    do{
+
+        Aluno::remover_aluno(alunos);
+
+        BancoDeDados::saveDados(alunos);
+        cout << "Dados atualizados" << endl;
+        cout << "------------------------------------" << endl;
+
+        cout << "Alunos atualizado " << endl;
+        for(Aluno aluno : alunos){
+            cout << aluno.getNome() << ", " << aluno.getEmail() << ", " 
+                << aluno.getNota1() << ", " << aluno.getNota2() << endl;
+        }
+
+        cout << "deseja remover outro aluno?" << endl;
+        cin >> resposta;
+
+    }while(resposta == 's');
+
+    
+
     return 0;
 }
 
