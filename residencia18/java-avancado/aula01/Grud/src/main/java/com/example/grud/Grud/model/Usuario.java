e1986a02854e8545ab609cff4eaeb8c0848358f3
mikaelle1992 <mikaelle.rubiaps02@gmail.com>
2024-03-12 22:51:54 -0300

    aula 01

---
+++ residencia18/java-avancado/aula01/Grud/src/main/java/com/example/grud/Grud/model/Usuario.java
@@ -0,0 +1,54 @@
+package com.example.grud.Grud.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Usuario {
+	
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String email;
+	private String senha;
+	
+	
+	public Usuario() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Usuario(Long id, String name, String email, String senha) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.senha = senha;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+}
