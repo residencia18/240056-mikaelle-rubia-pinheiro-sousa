a377dbf1a4c901616922119ecdd91d1f1c7bfc3f
mikaelle1992 <mikaelle.rubiaps02@gmail.com>
2024-03-14 14:03:27 -0300

    fix: relacionamento de carro com usuario

--- residencia18/java-avancado/aula02/Grud/src/main/java/com/example/grud/Grud/model/Usuario.java
+++ residencia18/java-avancado/aula02/Grud/src/main/java/com/example/grud/Grud/model/Usuario.java
@@ -4,62 +4,74 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.OneToMany;
+import java.util.List;
 
 @Entity
 public class Usuario {
-	
-	@Id  @GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	private String name;
-	private String email;
-	private String senha;
-	
-	
-	public Usuario() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-	public Usuario(Long id, String name, String email, String senha) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.email = email;
-		this.senha = senha;
-	}
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	 public void setName(String name) throws Exception {
-		if(name.length() < 3) {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String senha;
+    
+    @OneToMany(mappedBy = "proprietario")
+    private List<Carro> carros;
+    
+    public Usuario() {
+        super();
+    }
+    
+    public Usuario(Long id, String name, String email, String senha) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.senha = senha;
+    }
+    
+    public Long getId() {
+        return id;
+    }
+    
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) throws Exception {
+        if (name.length() < 3) {
             Exception e = new Exception("Nome deve ter mais de 2 caracteres");
             throw e;
-		}
-		this.name = name;
-		}
-
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getSenha() {
-		return senha;
-	}
-	public void setSenha(String senha) throws Exception {
-		if(senha.length() < 6) {
-            Exception e = new Exception("senha deve ter mais de 7 caracteres");
-            throw e;
-		}
-		this.senha = senha;
-	}
-	
-	
+        }
+        this.name = name;
+    }
+    
+    public String getEmail() {
+        return email;
+    }
+    
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    public String getSenha() {
+        return senha;
+    }
+    
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
+    public List<Carro> getCarros() {
+        return carros;
+    }
+    
+    public void setCarros(List<Carro> carros) {
+        this.carros = carros;
+    }
 }
