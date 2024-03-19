a7461695feee1939a39cfdd44c8fe3b659e68625
mikaelle1992 <mikaelle.rubiaps02@gmail.com>
2024-03-19 15:04:42 -0300

    feature: teste integrado

---
+++ residencia18/java-avancado/aula03/Grud/src/main/java/com/example/grud/Grud/controller/Form/UsuarioForm.java
@@ -0,0 +1,64 @@
+package com.example.grud.Grud.controller.Form;
+
+import java.util.List;
+
+import com.example.grud.Grud.controller.dto.CarroDTO;
+import com.example.grud.Grud.model.Carro;
+import com.example.grud.Grud.model.Usuario;
+
+import jakarta.persistence.OneToMany;
+
+public class UsuarioForm {
+	private String name;
+	private String email;
+	private String senha;
+	
+    private List<Carro> carros;
+    
+	public UsuarioForm( ) {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public UsuarioForm(String name, String email, String senha, List<Carro> carros) {
+		super();
+		this.name = name;
+		this.email = email;
+		this.senha = senha;
+		this.carros= carros;
+	}
+
+	public List<Carro> getCarros() {
+		return carros;
+	}
+
+	public void setCarros(List<Carro> carros) {
+		this.carros = carros;
+	}
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+	public Usuario criarUsuario() {
+		return new Usuario(null, name, email, senha, carros);
+		
+	}
+	
+	
+}
