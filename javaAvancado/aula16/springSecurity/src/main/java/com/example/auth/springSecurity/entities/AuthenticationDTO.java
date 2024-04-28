b75a6cafa5355fc104228e79fa4ccbdc2516e83e
mikaelleRubia <mikaelle.rubia@outlook.com>
2024-04-28 18:51:22 -0300

    fix: remoção de pasta

--- javaAvancado/aula16/springSecurity/src/main/java/com/example/auth/springSecurity/entities/AuthenticationDTO.java
+++
@@ -1,4 +0,0 @@
-package com.example.auth.springSecurity.entities;
-
-public record AuthenticationDTO(String login, String password) {
-}
--- javaAvancado/aula16/springSecurity/src/main/java/com/example/auth/springSecurity/entities/RegisterDTO.java
+++
@@ -1,4 +0,0 @@
-package com.example.auth.springSecurity.entities;
-
-public record RegisterDTO(String login, String password, String role) {
-}
