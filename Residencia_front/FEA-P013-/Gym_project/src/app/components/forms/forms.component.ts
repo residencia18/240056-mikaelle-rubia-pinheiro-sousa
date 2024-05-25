309d54a83f265bef14591b57f23c95d92b855ddd
mikaelleRubia <mikaelle.rubia@outlook.com>
2024-05-25 17:22:16 -0300

    feature:filter result

--- Residencia_front/FEA-P013-/Gym_project/src/app/components/forms/forms.component.ts
+++ Residencia_front/FEA-P013-/Gym_project/src/app/components/forms/forms.component.ts
@@ -1,3 +1,4 @@
+import { Location } from './../../types/location.interface';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { GetUnitsService } from '../../services/get-units.service';
@@ -11,7 +12,8 @@ import { GetUnitsService } from '../../services/get-units.service';
   styleUrls: ['./forms.component.css']
 })
 export class FormsComponent implements OnInit {
-  results = [];
+  results: Location[] = [];
+  filteredResults: Location[] = [];
   formGroup!: FormGroup;
 
   constructor(private formBuilder: FormBuilder, private getUnitsService: GetUnitsService) {}
@@ -19,7 +21,11 @@ export class FormsComponent implements OnInit {
   ngOnInit(): void {
     this.formGroup = this.formBuilder.group({
       hour: '',
-      showClosed: false
+      showClosed: true
+    });
+    this.getUnitsService.getAllUnits().subscribe(data =>{
+      this.results = data.locations;
+      this.filteredResults = data.locations;
     });
   }
 
@@ -29,7 +35,11 @@ export class FormsComponent implements OnInit {
   }
 
   onSubmit() {
-    console.log("clear", this.formGroup.value);
-    this.getUnitsService.getAllUnits().subscribe(data => console.log(data));
+    console.log("clear", this.filteredResults);
+    if(!this.formGroup.value.showClosed){
+      this.filteredResults = this.results.filter(location=> location.opened === true)
+    }else{
+      this.filteredResults = this.results;
+    }
   }
 }
