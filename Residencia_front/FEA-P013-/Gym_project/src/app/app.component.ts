65267f1b06a8eace36b3e969202e82560772c7bb
mikaelleRubia <mikaelle.rubia@outlook.com>
2024-05-25 17:02:14 -0300

    feature:  create get unit service

--- Residencia_front/FEA-P013-/Gym_project/src/app/app.component.ts
+++ Residencia_front/FEA-P013-/Gym_project/src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 import { FormsComponent } from './components/forms/forms.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
-    styleUrl: './app.component.css',
+    styleUrls: ['./app.component.css'],
     imports: [RouterOutlet, HeaderComponent, FormsComponent]
 })
 export class AppComponent {
--- Residencia_front/FEA-P013-/Gym_project/src/app/components/forms/forms.component.ts
+++ Residencia_front/FEA-P013-/Gym_project/src/app/components/forms/forms.component.ts
@@ -1,33 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { GetUnitsService } from '../../services/get-units.service';
+
 
 @Component({
   selector: 'app-forms',
   standalone: true,
   imports: [ReactiveFormsModule],
   templateUrl: './forms.component.html',
-  styleUrl: './forms.component.css'
+  styleUrls: ['./forms.component.css']
 })
 export class FormsComponent implements OnInit {
   results = [];
   formGroup!: FormGroup;
 
-  constructor(private formBuilder:FormBuilder ){}
+  constructor(private formBuilder: FormBuilder, private getUnitsService: GetUnitsService) {}
 
   ngOnInit(): void {
     this.formGroup = this.formBuilder.group({
-      hour:'',
-      showClosed:false
-    })
+      hour: '',
+      showClosed: false
+    });
   }
 
   onClean() {
     this.formGroup.reset();
-    console.log("clear")
+    console.log("clear");
   }
 
   onSubmit() {
-    console.log("clear", this.formGroup.value)
+    console.log("clear", this.formGroup.value);
+    this.getUnitsService.getAllUnits().subscribe(data => console.log(data));
   }
 }
-
