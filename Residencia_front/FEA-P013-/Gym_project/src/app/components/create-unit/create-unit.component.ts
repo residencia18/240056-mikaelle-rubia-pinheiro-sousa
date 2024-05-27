efbfbd004efa7a1bee8a02ef69cf1cba9c7a163c
mikaelleRubia <mikaelle.rubia@outlook.com>
2024-05-26 22:39:32 -0300

    feature: componente para criar Unidade

--- Residencia_front/FEA-P013-/Gym_project/src/app/components/create-unit/create-unit.component.ts
+++ Residencia_front/FEA-P013-/Gym_project/src/app/components/create-unit/create-unit.component.ts
@@ -1,29 +1,89 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CreateUnitsService } from '../../services/create-units.service';
-import { catchError, of } from 'rxjs';
+import { schedule } from '../../../environments/environments';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from '../footer/footer.component';
+import {Location, Schedule} from '../../../app/types/location.interface';
+import { MessageService } from 'primeng/api';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-unit',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule, HeaderComponent, FooterComponent],
+  providers: [MessageService],
   templateUrl: './create-unit.component.html',
   styleUrl: './create-unit.component.css'
 })
 export class CreateUnitComponent {
-  newUnitForm!: FormGroup;
+public scheduleList:Schedule [] =[]
+public updateType = false;
+public unitUpadate: Location | null = null;
+public id: string = '';
 
-  constructor(private formBuilder: FormBuilder, private createUnitsService: CreateUnitsService) {}
+  newUnitForm!: FormGroup;
+  public hours: string[] = schedule.hour;
+  public weekdays: string[] =schedule.weekdays;
+  constructor(private formBuilder: FormBuilder,
+     private createUnitsService: CreateUnitsService, private route: ActivatedRoute, private router: Router,) {}
 
   ngOnInit(): void {
-    this.newUnitForm = this.formBuilder.group({
-      title: [''],
-      content: [''],
-      mask: ['not required'],
-      towel: ['not required'],
-      fountain: ['not required'],
-      lockerRoom: ['open']
+    this.route.paramMap.subscribe(params => {
+      const id = params.get('id');
+      this.id = id !== null ? id : '';
     });
+
+    this.newUnitForm = this.formBuilder.group({
+      title: ['', { validators: [Validators.required], updateOn: 'blur' }],
+      content: ['', { validators: [Validators.required], updateOn: 'blur' }],
+      mask: ['recommended', { validators: [Validators.required], updateOn: 'blur' }],
+      towel: ['recommended', { validators: [Validators.required], updateOn: 'blur' }],
+      fountain: ['partial', { validators: [Validators.required], updateOn: 'blur' }],
+      lockerRoom: ['partial', { validators: [Validators.required], updateOn: 'blur' }],
+      opened: ['Fechado', { validators: [Validators.required], updateOn: 'blur' }],
+      hour:['06h às 12h', { validators: [Validators.required], updateOn: 'blur' }],
+      weekdaysSelect:['Seg. à Sex.', { validators: [Validators.required], updateOn: 'blur' }]
+    });
+
+
+    this.updateType = this.id != '' ? true: false;
+
+    if (this.updateType){
+
+      this.filterId(this.id)
+
+      this.newUnitForm = this.formBuilder.group({
+        title: [this.unitUpadate?.title, { validators: [Validators.required], updateOn: 'blur' }],
+        content: [this.unitUpadate?.content, { validators: [Validators.required], updateOn: 'blur' }],
+        mask: [this.unitUpadate?.mask, { validators: [Validators.required], updateOn: 'blur' }],
+        towel: [this.unitUpadate?.towel, { validators: [Validators.required], updateOn: 'blur' }],
+        fountain: [this.unitUpadate?.fountain, { validators: [Validators.required], updateOn: 'blur' }],
+        lockerRoom: [this.unitUpadate?.locker_room, { validators: [Validators.required], updateOn: 'blur' }],
+        opened: [this.unitUpadate?.opened, { validators: [Validators.required], updateOn: 'blur' }],
+        hour:['06h às 12h', { validators: [Validators.required], updateOn: 'blur' }],
+        weekdaysSelect:['Seg. à Sex.', { validators: [Validators.required], updateOn: 'blur' }]
+      });
+    }
+
+  }
+
+  filterId(id: string): void {
+    this.createUnitsService.getFilteredUnitById(id)
+      .subscribe({
+        next: (response) => {
+          if (response) {
+            this.unitUpadate = response;
+            console.log("this.unitUpadate:", this.unitUpadate);
+          } else {
+            console.error('Unit not found for ID:', id);
+          }
+        },
+        error: (err) => {
+          console.error('Error fetching filtered units:', err);
+        }
+      });
   }
 
   onSubmit(): void {
@@ -31,36 +91,101 @@ export class CreateUnitComponent {
       const UnitData = {
         title: this.newUnitForm.value?.title,
         content: this.newUnitForm.value?.content,
-        opened: true,
+        opened: this.newUnitForm.value?.opened == "true" ? true : false,
         mask: this.newUnitForm.value?.mask,
         towel: this.newUnitForm.value?.towel,
         fountain: this.newUnitForm.value?.fountain,
-        locker_room: this.newUnitForm.value?.lockerRoom
+        locker_room: this.newUnitForm.value?.lockerRoom,
+        schedules:this.scheduleList
       };
       console.log('UnitData:', UnitData);
-      this.createUnitsService.addUnit(UnitData)
-        .pipe()
-        .subscribe({
-          next: (response) => {
-            if (response) {
-              console.log('Adicionado com sucesso:', response);
-            }
-          },
-          error: (err) => {
-            console.error('Error ao adicionar unidade:', err);
-          }
-        });
+      this.addUnits(UnitData);
+      this.updateType = false;
     } else {
       console.error('Formulário inválido');
+      alert('everything is broken!');
+    }
+}
+  onSubmitUpadate(): void {
+    if (this.newUnitForm.valid) {
+      const UnitData = {
+        title: this.newUnitForm.value?.title,
+        content: this.newUnitForm.value?.content,
+        opened:this.newUnitForm.value?.opened == "true" || this.newUnitForm.value?.opened == true ? true : false,
+        mask: this.newUnitForm.value?.mask,
+        towel: this.newUnitForm.value?.towel,
+        fountain: this.newUnitForm.value?.fountain,
+        locker_room: this.newUnitForm.value?.lockerRoom,
+        schedules:this.scheduleList
+      };
+      console.log('UnitData:', UnitData);
+      this.updateUnits(UnitData, this.id);
+    } else {
+      console.error('Formulário inválido');
+      alert('everything is broken!');
+    }
+  }
+
+  addUnits(data: any){
+    this.createUnitsService.addUnit(data)
+    .pipe()
+    .subscribe({
+      next: (response) => {
+        if (response) {
+          console.log('Adicionado com sucesso:', response);
+          alert('Added successfully!');
+
+        }
+      },
+      error: (err) => {
+        console.error('Error ao adicionar unidade:', err);
+      }
+    });
+    this.onClear();
+  }
+
+  updateUnits(data: any, id: string){
+    this.createUnitsService.UpdateUnit(data, id)
+    .pipe()
+    .subscribe({
+      next: (response) => {
+        if (response) {
+          console.log('Atualizado com sucesso:', response);
+          alert('Update successfully!');
+          this.router.navigate(['/getUnits']);
+        }
+      },
+      error: (err) => {
+        console.error('Error ao adicionar unidade:', err);
+      }
+    });
+    this.onClear();
+  }
+
+  addSchedules() {
+    const selectedWeekday = this.newUnitForm.get('weekdaysSelect')?.value;
+    const selectedHour = this.newUnitForm.get('hour')?.value;
+    const exists = this.scheduleList.some(schedule => schedule.weekdays === selectedWeekday);
+
+    if (!exists) {
+      const newSchedule: Schedule = { weekdays: selectedWeekday, hour: selectedHour };
+      this.scheduleList.push(newSchedule);
+      console.log('Schedule added:', this.scheduleList);
     }
   }
 
   onClear() {
     this.newUnitForm.reset({
-      mask: 'not required',
-      towel: 'not required',
-      fountain: 'not required',
-      lockerRoom: 'open'
+      title:'',
+      content: '',
+      mask: 'recommended',
+      towel: 'recommended',
+      fountain: 'partial',
+      lockerRoom: 'partial',
+      opened:'Fechado',
+      hour:'06h às 12h',
+      weekdaysSelect:'Seg. à Sex.'
     });
   }
 }
+
--- Residencia_front/FEA-P013-/Gym_project/src/app/services/create-units.service.ts
+++ Residencia_front/FEA-P013-/Gym_project/src/app/services/create-units.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environments';
 import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { UnitsResponse } from '../types/units-response.interface';
-import { Location } from '../types/location.interface';
+import { BehaviorSubject, Observable, map } from 'rxjs';
+import { Location, Schedule } from '../types/location.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -12,18 +11,19 @@ export class CreateUnitsService {
   private allUnitsSubject: BehaviorSubject<Location[]> = new BehaviorSubject<Location[]>([]);
   private allUnits$: Observable<Location[]> = this.allUnitsSubject.asObservable();
   private filteredUnits: Location[] = [];
-  results: Location[] = [];
   filteredResults: Location[] = [];
 
   readonly apiUrl = environment.fireDatabase;
-  constructor(private http: HttpClient) {
-    this.http.get<UnitsResponse>(this.apiUrl).subscribe(data =>{
-    this.allUnitsSubject.next(data.locations);
-    this.filteredUnits = data.locations;
-  });}
+  constructor(private http: HttpClient) {}
 
   getAllUnits(): Observable<Location[]> {
     return this.allUnits$;
+  }
+
+  getFilteredUnitById(id: string): Observable<Location | undefined> {
+    return this.getAllUnits().pipe(
+      map(units => units.find(unit => unit.id === id))
+    );
   }
 
   getFilteredUnits() {
@@ -34,15 +34,48 @@ export class CreateUnitsService {
     return this.filteredUnits = value;
   }
 
+  fetchAllUnits(): Observable<Location[]> {
+    return this.http.get<Location[]>(this.apiUrl + 'unitGym.json').pipe(
+      map((responseData) => {
+        const postArray: Location[] = [];
+        for (const key in responseData) {
+          if (responseData.hasOwnProperty(key)) {
+            postArray.push({ ...(responseData as any)[key], id: key });
+          }
+        }
+        this.allUnitsSubject.next(postArray);
+        this.filteredUnits = postArray;
+        return postArray;
+      })
+    );
+  }
+
   addUnit(LocationData: {
-    title: string,
-    content: string,
-    opened: boolean,
-    mask: string,
-    towel: string,
-    fountain: string,
-    locker_room: string,
-  }): Observable<any> {
-  return this.http.post(this.apiUrl+ 'unitGym.json', LocationData);
+              title: string,
+              content: string,
+              opened: boolean,
+              mask: string,
+              towel: string,
+              fountain: string,
+              locker_room: string,
+              schedules: Schedule []}): Observable<any> {
+    return this.http.post(this.apiUrl+ 'unitGym.json', LocationData);
+  }
+
+  UpdateUnit(LocationData: {
+                  title: string,
+                  content: string,
+                  opened: boolean,
+                  mask: string,
+                  towel: string,
+                  fountain: string,
+                  locker_room: string,
+                  schedules: Schedule []}, idUnit: string): Observable<any> {
+          return this.http.put(this.apiUrl+ `unitGym/${idUnit}.json`, LocationData);
+  }
+
+
+  deleteUnit(idUnit: string):Observable<any>{
+    return this.http.delete(this.apiUrl+ `unitGym/${idUnit}.json`,);
   }
 }
